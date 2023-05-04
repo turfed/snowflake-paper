b5a6848392ba537bb6f360822d036e4b11c7aadb
David Fifield <david@bamsoftware.com>
2023-05-04 15:13:40 +0000

    Add draft users-ir and users-tm graphs.

--- figures/users/users-country.r
+++ figures/users/users-country.r
@@ -1,19 +1,14 @@
 # Makes a graph showing the estimated number of simultaneous Snowflake users
-# in Russia, around the time of blocking events.
+# in various countries, around the time of respective blocking events.
 #
 # Usage:
-#   Rscript users-country.r userstats-bridge-combined-multi.csv users-ru.pdf
+#   Rscript users-country.r userstats-bridge-combined-multi.csv
 
 library("tidyverse")
 
 source("../common.r")
 
-DATE_LIMITS <- lubridate::ymd(c(
-	"2021-11-01",
-	# 2021-12-01 first blocking https://bugs.torproject.org/tpo/community/support/40050
-	# 2022-02-15 release of Tor Browser 12.0.3 with altered fingerprint https://bugs.torproject.org/tpo/anti-censorship/censorship-analysis/40030#note_2893870
-	"2023-03-15"
-))
+HEIGHT <- 1.5
 
 WANTED_FINGERPRINTS <- c(
 	"7659DA0F96B156C322FBFF3ACCC9B9DC01C27C73" = "snowman",
@@ -26,13 +21,20 @@ date_labels <- function(breaks) {
 	strftime(breaks, ifelse(!is.na(breaks) & lubridate::month(breaks) == 1, "%b\n%Y", "%b"), tz = "UTC")
 }
 
+# Return a one-letter abbreviation for the month, followed by a year for January only.
+date_labels_abbrev <- function(breaks) {
+	paste0(
+		substr(strftime(breaks, "%b", tz = "UTC"), 1, 1),
+		ifelse(!is.na(breaks) & lubridate::month(breaks) == 1, strftime(breaks, "\n%Y", tz = "UTC"), "")
+	)
+}
+
 (function() {
 	args <- commandArgs(trailingOnly = TRUE)
-	if (length(args) != 2) {
-		stop("usage: Rscript users-country.r userstats-bridge-combined-multi.csv users-ru.pdf")
+	if (length(args) != 1) {
+		stop("usage: Rscript users-country.r userstats-bridge-combined-multi.csv")
 	}
 	bridge_combined_multi_csv_path <<- args[[1]]
-	output_path <<- args[[2]]
 })()
 
 bridge_combined_multi <- read_csv(bridge_combined_multi_csv_path, comment = "#") %>%
@@ -44,7 +46,7 @@ bridge_combined_multi <- read_csv(bridge_combined_multi_csv_path, comment = "#")
 	mutate(
 		across(c(low, high), ~ .x * frac / 100),
 		frac = NULL,
-		users = (low + high) / 2
+		users = pmin(low, high) # Take the minimum, not the mean, because "low" is too noisy, especially in Turkmenistan.
 	)
 
 bridge_combined <- bridge_combined_multi %>%
@@ -60,22 +62,47 @@ bridge_combined <- bridge_combined_multi %>%
 # TODO: assign country "??" proportionally to other countries.
 # "WS.makeWebsocket ignores params (i.e. `client_ip`), losing country statistics" https://bugs.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake-webext/82
 
-p <- ggplot() +
-	# Data series.
-	geom_line(
-		data = bridge_combined %>% filter(country == "ru"),
-		aes(x = date, y = users),
-		size = LINE_SIZE
-	) +
-
-	scale_x_date(
-		date_breaks = "1 month",
-		minor_breaks = NULL,
-		labels = date_labels
-	) +
-	coord_cartesian(xlim = DATE_LIMITS, expand = FALSE) +
-
-	COMMON_THEME +
-	theme(plot.margin = unit(c(0, 0, 0, 0), "mm")) +
-	labs(x = NULL, y = NULL)
-ggsave(output_path, p, width = DOCUMENT_LINEWIDTH, height = 1.5)
+for (g in list(
+	list(
+		country = "ru",
+		date_limits = lubridate::ymd(c(
+			"2021-11-01",
+			# 2021-12-01 first blocking https://bugs.torproject.org/tpo/community/support/40050
+			# 2022-02-15 release of Tor Browser 12.0.3 with altered fingerprint https://bugs.torproject.org/tpo/anti-censorship/censorship-analysis/40030#note_2893870
+			"2023-03-31"
+		)),
+		date_labels = date_labels
+	),
+	list(
+		country = "ir",
+		date_limits = lubridate::ymd(c(
+			"2022-09-01",
+			"2023-04-30"
+		)),
+		date_labels = date_labels
+	),
+	list(
+		country = "tm",
+		date_limits = lubridate::ymd(c(
+			"2020-10-01",
+			"2022-09-30"
+		)),
+		date_labels = date_labels_abbrev
+	)
+)) {
+	p <- ggplot() +
+		geom_line(
+			data = bridge_combined %>% filter(country == g$country),
+			aes(x = date, y = users),
+			size = LINE_SIZE
+		) +
+		scale_x_date(
+			date_breaks = "1 month",
+			minor_breaks = NULL,
+			labels = g$date_labels
+		) +
+		coord_cartesian(xlim = g$date_limits, expand = FALSE) +
+		COMMON_THEME +
+		labs(x = NULL, y = NULL)
+	ggsave(sprintf("users-%s.pdf", g$country), p, width = DOCUMENT_LINEWIDTH, height = HEIGHT)
+}
